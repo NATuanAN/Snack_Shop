2c842851effe10c4f44e296d6c8c6ad5da6be689
Nguyen Anh Tuan <22521604@gm.uit.edu.vn>
2025-12-10 15:46:47 +0700

    Init project

---
+++ src/main/java/com/Snack_BE/SnackBeApplication.java
@@ -0,0 +1,13 @@
+package com.Snack_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnackBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnackBeApplication.class, args);
+	}
+
+}
